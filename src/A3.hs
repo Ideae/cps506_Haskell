ebfce244d48d3cb62cca9cf36b67778cb867f73f
Ideae <zack.tibia@gmail.com>
2015-03-08 16:21:28 -0700

    fully commented

--- src/A3.hs
+++ src/A3.hs
@@ -7,8 +7,9 @@ module A3 where
     import Data.List.Split
     import Data.Char
     import Data.Data
-    data PieceType = Pawn | Rook | Knight | Bishop | Queen | King deriving (Data, Typeable)
-    
+    --main data type for piece types
+    data PieceType = Pawn | Rook | Knight | Bishop | Queen | King deriving (Data, Typeable, Eq)
+    --show instance for piece types
     instance Show PieceType where
         show Pawn   = "P "
         show Rook   = "R "
@@ -16,35 +17,23 @@ module A3 where
         show Bishop = "B "
         show Queen  = "Q "
         show King   = "K "
-    instance Eq PieceType where
-        Pawn == Pawn   = True
-        Rook == Rook   = True
-        Knight == Knight = True
-        Bishop == Bishop = True
-        Queen == Queen  = True
-        King == King   = True
-        _ == _ = False
-
-    data Color = Black | White deriving (Data, Typeable)
-
+    --data type for colors
+    data Color = Black | White deriving (Data, Typeable, Eq)
+    --show instance for colors
     instance Show Color where
         show Black = "B"
         show White = "W"
-    instance Eq Color where
-        Black == Black = True
-        White == White = True
-        _ == _ = False
-        x /= y = not (x == y)
-    
+    --function to read in a color from a char
     readColor :: Char -> Color
     readColor 'B' = Black
     readColor 'W' = White
-
-    
+    --cell datatype, to represent a piece (a color and a piecetype), or none (no piece)
     data Cell = Piece (Color, PieceType) | None
+    --show instance for cells
     instance Show Cell where
         show None = "   "
         show (Piece (x,y)) = show x ++ show y
+    --read instance for cells, to read in the piece info from strings
     instance Read Cell where
         readsPrec _ "" = [(None,"")]
         readsPrec _ (h:t) = [(Piece ((if h == 'B' then Black else if h == 'W' then White else error ("Parse error")), readPieceType t), "")] where
@@ -55,28 +44,34 @@ module A3 where
                             | a == "Q" = Queen   
                             | a == "K" = King    
                             | otherwise = error ("Parse Error" ++ a)
-
+    --gets the piecetype from a cell
     getCellPiece :: Cell -> PieceType
     getCellPiece None = error "cell does not contain piece"
     getCellPiece (Piece (c,p)) = p
+    --gets the color from a cell
     getCellColor :: Cell -> Color
     getCellColor None = error "cell does not contain piece"
     getCellColor (Piece (c,p)) = c
+    --checks if a cell is empty
     isCellEmpty None = True
     isCellEmpty _ = False
-
+    --data type representing the full board, a 2d array a Cells, along with two Cell lists for captured pieces
     data Board = Board [[Cell]] [Cell] [Cell]
-
+    --extracts the grid from a board object
     getBoardGrid (Board grid _ _) = grid
+    --extracts the white captures from a board object
     getBoardWCaptures (Board _ w _) = w
+    --extracts the black captures from a board object
     getBoardBCaptures (Board _ _ b) = b
+    --adds a captured piece to the board to the approapriate list
     addCapture (Board br w b) (Piece (c,p)) = if c == Black then (Board br (w ++ [Piece (c,p)]) b) else (Board br w (b ++ [Piece (c,p)]))
-
+    --type synonym representing a coordinate on the board
     type Coord = (Int,Int)
+    --type synonym representing a direction to travel on the board (derived from a direction string, such as NW)
     type Dir = (Int,Int)
-
+    --the general form for the back row of a chess board
     backRow = ["R","Kn","B","Q","K","B","Kn","R"]
-    
+    --the chess board containing strings to be read in to produce a Board
     chessBoardString = [
                     map ("W" ++) backRow,
                     replicate 8 "WP",
@@ -86,54 +81,51 @@ module A3 where
                     map ("B" ++) backRow
                   ]
 
-    parseRow :: [String] -> [Cell]
-    parseRow [] = []
-    parseRow (x:y) = (read x :: Cell) : (parseRow y)
-
+    --parses a full board from string input
     parseBoard :: [[String]] -> Board
     parseBoard [] = (Board [] [] [])
     parseBoard x = (Board (parse x) [] [])
-                where parse [] = []
+                where parseRow [] = []
+                      parseRow (x:y) = (read x :: Cell) : (parseRow y)
+                      parse [] = []
                       parse (x:y) = (parseRow x) : (parse y)
-
+    --creates the bar string from two characters
     makeBarString x y = " " ++ take (8 * 4 + 1) (cycle (x ++ take 3 (y++y++y))) ++ "\n"
     barString = makeBarString "o" "~"
+    --makes the bottom row string of letters
     makeLetterString :: Int -> String
     makeLetterString 0 = ""
     makeLetterString x = (makeLetterString (x-1)) ++ "   " ++ [(chr ((ord 'a') + x - 1))]
 
-    showRow :: [Cell] -> String
-    showRow [] = "|"
-    showRow (x:y) = "|" ++ (show x) ++ showRow y
-
+    --shows the board as a string
     showBoard :: Board -> String
     showBoard (Board [] _ _) = barString
     showBoard (Board (x:y) a b) = showBoard (Board y a b) ++ (show (8 - length y)) ++ showRow x ++ "\n" ++ barString ++ (if (length y) == 7 then (makeLetterString 8 ++ "\n") else "")
-
+                                    where showRow [] = "|"
+                                          showRow (x:y) = "|" ++ (show x) ++ showRow y
+    --converts a character into an index
     charIndex :: Char -> Int
     charIndex c = ord c - ord 'a'
-
-    dirMapping :: Char -> (Int,Int)
+    --maps a direction string to a (x,y) direction
+    dirMapping :: Char -> Dir
     dirMapping 'N' = (0,1)
     dirMapping 'S' = (0,-1)
     dirMapping 'W' = (-1,0)
     dirMapping 'E' = (1,0)
     dirMapping _ = (0,0)
-    
-    dirOffset :: String -> (Int, Int)
+    --converts a direction string into a Dir
+    dirOffset :: String -> Dir
     dirOffset "" = (0,0)
     dirOffset (h:t) = addOffset (dirMapping h) (dirOffset t)
                     where addOffset (x,y) (z,w) = (x+z,y+w)
-    
-    posCoord :: String -> (Int, Int)
+    --converts a position string into a Coord (such as g7)
+    posCoord :: String -> Coord
     posCoord "" = (-1,-1)
     posCoord s = (charIndex (s!!0), digitToInt (s!!1) - 1)
-
-    
-
+    --gets a cell on the board from a Coord
     getCell :: Coord -> Board -> Cell
     getCell (cx,cy) (Board x _ _) = (x !! cy) !! cx
-
+    --checks if a move is valid based on the current Cell, the direction, and the Cell's Piecetype
     isMoveValid :: Cell -> Dir -> Bool
     isMoveValid (Piece (c,p)) (a,b) | p == Knight = (x == 2 && y == 1) || (x == 1 && y == 2)
                                     | x > 1 || y > 1 = False
@@ -144,18 +136,19 @@ module A3 where
                                     | p == Pawn = (x == 0 && y == 1 && (c /= Black || b == (-1)) && (c /= White || b == 1))
                                     where x = abs a
                                           y = abs b
-
+    --replaces an element in a list for another element
     replace :: Int -> x -> [x] -> [x]
     replace n e l = let (xs,ys) = splitAt n l in xs ++ [e] ++ (tail ys)
-
+    --replaces a Cell in the Board at a certain Coord
     replaceInGrid :: Coord -> Cell -> Board -> Board
     replaceInGrid (cx,cy) elem (Board grid x y) = let row = grid !! cy
                                                       newrow = replace cx elem row
                                                       in (Board (replace cy newrow grid) x y)
-
+    --moves a piece from one Coord to another Coord
     moveSingle :: Coord -> Coord -> Board -> Board
     moveSingle (cx,cy) (c2x,c2y) grid = replaceInGrid (cx,cy) None (replaceInGrid (c2x,c2y) (getCell (cx,cy) grid) grid)
-
+    --master function to attempt a move based on user input. checks the bounds, check if the cell is empty, checks the contents of the destination cell,
+    --makes sure that the move is valid, and calls the makeMovePawn function if the piece is a Pawn
     makeMove :: Coord -> Dir -> Int -> Board -> Board
     makeMove _ _ 0 board = board
     makeMove (cx,cy) (dx,dy) n board = if (ax < 0 || ax >= 8 || ay < 0 || ay >= 8) then board else
@@ -172,6 +165,7 @@ module A3 where
                                                                 else moveSingle (cx,cy) (ax,ay) (addCapture board (Piece(col2,p2)))
                                        where ax = cx + dx
                                              ay = cy + dy
+    --makeMove helper funciton to handle Pawn special cases. the pawn can only move forward (but cannot capture forward), or diagnoal if it is capturing, and two steps on it's first move
     makeMovePawn :: Coord -> Dir -> Int -> Cell -> Cell -> Board -> Board
     makeMovePawn (cx,cy) (dx,dy) n origcell nextcell board = let col = getCellColor origcell
                                                                  col2 = getCellColor nextcell
@@ -184,11 +178,11 @@ module A3 where
                                                                  if (col2 == col) then board else moveSingle (cx,cy) (ax,ay) (addCapture board nextcell)
                                                                  where ax = cx+dx
                                                                        ay = cy+dy
-
+    --shows a list of pieces, used to show the captured pieces
     showPieces :: [Cell] -> String
     showPieces [] = []
     showPieces ((Piece(h1,h2)):t) = (show $ toConstr h1) ++ (show $ toConstr h2) ++ (if length t /= 0 then "," else "") ++ showPieces t
-
+    --the input loop that takes in input on standard in to modif
     inputLoop (Board brd w b) = do
         input <- getLine
         if input == ""
